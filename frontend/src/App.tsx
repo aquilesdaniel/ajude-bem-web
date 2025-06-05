7d9d2f361969db33f5475fe22e3d5ed36eebed1f
aquilesdaniel <aquilesvdaniel@gmail.com>
2025-06-05 20:30:27 -0300

    adicionando axios e mais um componente

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router";
 import Home from "./screens/Home";
+import Login from "./screens/Login";
 
 function App() {
   console.log("App component rendered");
@@ -7,6 +8,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </>
   );
---
+++ frontend/src/screens/Login.tsx
@@ -0,0 +1,9 @@
+function Login() {
+  return (
+    <>
+      <h1>Receba</h1>
+    </>
+  );
+}
+
+export default Login;
